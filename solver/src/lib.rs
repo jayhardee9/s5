9126ef8044d9c47cd12032be028f633554162533
Jerry Hardee <hardeejj9@gmail.com>
2019-11-10 14:03:58 -0600

    Removed placeholder test

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -24,11 +24,3 @@ pub mod equations;
 pub mod formulas;
 pub mod solver_state;
 pub mod variables;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
